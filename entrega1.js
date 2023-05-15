191abedbe0cdf6b3e6ab204a39a9e2976b1ecbc9
joacodiler <jdilernia99@gmail.com>
2023-05-15 11:41:37 -0300

    first commit

---
+++ entrega1.js
@@ -0,0 +1,54 @@
+
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(product) {
+        const productExists = this.products.some(p => p.code === product.code);
+        if (productExists) {
+            console.log('Ya existe un producto con ese código. Producto no agregado');
+            return;
+        }
+        const isEmpty = Object.values(product).some(x => x === null || x === '');
+        if (isEmpty) {
+            console.log('No se puede agregar un producto con campos vacíos');
+            return;
+        }
+        this.products.push(product);
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductByCode(code) {
+        const product = this.products.find(p => p.code === code);
+        if (!product) {
+            console.log('Not found');
+        }
+        return product;
+    }
+}
+
+const productManager = new ProductManager();
+
+const product1 = {
+    title: 'producto de prueba',
+    description: 'este es un producto de prueba',
+    price: 200,
+    thumbnail: 'sin imagen',
+    code: 'abc123',
+    stock: 25
+};
+
+
+
+productManager.addProduct(product1);
+
+console.log(productManager.getProducts());
+
+console.log(productManager.getProductByCode('abc123'));
+
+
+
